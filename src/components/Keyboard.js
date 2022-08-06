90ba3c090bc62a1f750292ec8b9ae6e7a075f681
Artem Sokolnikov <s9634331017@yandex.ru>
2022-08-06 14:25:11 +0300

    Fixed bugs: -each child-element has a unique "key" prop. -changed the color of the title when winning-losing; -removed the sensitivity of the guessed word to the case;

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -54,19 +54,19 @@ function Keyboard() {
   return (
     <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="line1">
-        {keys1.map((key) => {
-          return <Key keyVal={key} disabled={disabledLetters.includes(key)} />;
+        {keys1.map((key,index) => {
+          return <Key keyVal={key} key={index} disabled={disabledLetters.includes(key)} />;
         })}
       </div>
       <div className="line2">
-        {keys2.map((key) => {
-          return <Key keyVal={key} disabled={disabledLetters.includes(key)} />;
+        {keys2.map((key,index) => {
+          return <Key keyVal={key} key={index} disabled={disabledLetters.includes(key)} />;
         })}
       </div>
       <div className="line3">
         <Key keyVal={"ENTER"} bigKey />
-        {keys3.map((key) => {
-          return <Key keyVal={key} disabled={disabledLetters.includes(key)} />;
+        {keys3.map((key,index) => {
+          return <Key keyVal={key} key={index} disabled={disabledLetters.includes(key)} />;
         })}
         <Key keyVal={"DEL"} bigKey />
       </div>
